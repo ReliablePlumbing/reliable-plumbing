7560d86710a800edd0ec7fe5b9c5a0f30d20185f
mohamed sayed <mosayed93@gmail.com>
2018-01-18 02:19:46 +0200

    side menu changes

--- reliable-plumbing-client/src/app/admin-panel/admin-panel.component.ts
+++ reliable-plumbing-client/src/app/admin-panel/admin-panel.component.ts
@@ -96,15 +96,8 @@ export class AdminPanelComponent implements OnInit {
       this.currentSelectedTab = this.tabs.changePassword.index;
   }
 
-  op/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-  openNav() {
-    document.getElementById("mySidenav").style.width = "320px";
-    document.getElementById("main").style.marginLeft = "320px";
-  }
-
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-  closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+  toggleNav() {
+    document.getElementById("mySidenav").classList.toggle("opened");
+    document.getElementById("main").classList.toggle("opened");
   }
 }
